0621c27cf9e3530fc4b6ad435dac9d644645a9fc
HannesScript <mail@hannesscript.dev>
2024-05-03 15:01:00 +0200

    yeah!

---
+++ init.js
@@ -0,0 +1,9 @@
+function item(item, href, src ) {
+    const i = document.createElement(item);
+    i.defer = true;
+    i.href = href || '';
+    i.src = src || '';
+    document.head.appendChild(i);
+}
+
+item('link', 'https://cdn.jsdelivr.net/npm/melte@version/src/css/utility.css');
